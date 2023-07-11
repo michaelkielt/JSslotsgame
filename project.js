58c4198ef3106426abae73118610b863aabfea03
40156896 <michaelkielt@gmail.com>
2023-07-11 12:28:51 +0100

    added parsefFloat func to hold deposit as a float

--- project.js
+++ project.js
@@ -10,6 +10,7 @@ const prompt = require("prompt-sync")();
 
 const deposit = () => {
     const depositAmount = prompt("Enter a deposit amount: ")
+    const numberDepositAmount = parseFloat(depositAmount);
 }
 
 deposit();
