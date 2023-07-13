a5c15bc002822fd31398b85af34df049f9654709
40156896 <michaelkielt@gmail.com>
2023-07-13 19:40:26 +0100

    added transpose function

--- project.js
+++ project.js
@@ -78,8 +78,9 @@ const spin = () => {
         }
     }
 
-    const reels = [[],[],[]];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(random() * reelSymbols.length);
@@ -92,11 +93,21 @@ const spin = () => {
     return reels;
 };
 
+const transpose = (reels) => {
+    const rows = [];
 
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[i][j]);
+        }
+    }
 
-
+    return rows;
+}
 
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
