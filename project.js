4e1f6d55302ee21be62612aeb7a3be424feb73d4
40156896 <michaelkielt@gmail.com>
2023-07-14 12:03:05 +0100

    created getWinnings func to check user win

--- project.js
+++ project.js
@@ -115,14 +115,40 @@ const printRows = (rows) => {
                 rowString += " | ";
             }
         }
+        console.log(rowString);
     }
 }
 
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-console.log(reels);
-console.log(rows);
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+}
+
+const game = () => {
+    let balance = deposit();
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberOfLines);
+    console.log("You won, " + winnings.toString());
+}
+
+game();
